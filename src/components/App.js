d94eb0aa4a7bbe9b00335a19d7459dfd8f17a988
Silin Na <n1207n@gmail.com>
2017-02-09 21:41:33 -0800

    Initial project cleanup
    + Removed unnecessary react-slingshot example files
    
    + flow and sass-lint config files
    
    + Added some more npm dependencies

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,6 @@ class App extends React.Component {
       <div>
         <IndexLink to="/">Home</IndexLink>
         {' | '}
-        <Link to="/fuel-savings">Demo App</Link>
-        {' | '}
         <Link to="/about">About</Link>
         <br/>
         {this.props.children}
