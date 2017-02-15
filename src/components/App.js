bc64fb4e0b2d0b9c75e32bc06b52519000628244
Silin Na <n1207n@gmail.com>
2017-02-14 17:59:21 -0800

    feat: Added a React Boostrap Navbar component to start with
    + IndexLink for Brand Link
    
    + LinkContainer for NavItem

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { PropTypes } from 'react';
-import { Link, IndexLink } from 'react-router';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { IndexLink } from 'react-router';
+import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
 
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
@@ -7,12 +9,30 @@ import { Link, IndexLink } from 'react-router';
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <IndexLink to="/">Home</IndexLink>
-        {' | '}
-        <Link to="/about">About</Link>
-        <br/>
-        {this.props.children}
+      <div className="container">
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <IndexLink to="/">
+                React like-or-not
+              </IndexLink>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <LinkContainer to="/about">
+                <NavItem eventKey={2}>
+                  About
+                </NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <div className="content-container">
+          {this.props.children}
+        </div>
       </div>
     );
   }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-const HomePage = () => {
-  return (
-    <div>
-      <h1>React Slingshot</h1>
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-      <h2>Get Started</h2>
-      <ol>
-        <li>Review the <Link to="fuel-savings">demo app</Link></li>
-        <li>Remove the demo and start coding: npm run remove-demo</li>
-      </ol>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>React Slingshot</h1>
+
+        <h2>Get Started</h2>
+        <ol>
+          <li>Review the <Link to="fuel-savings">demo app</Link></li>
+          <li>Remove the demo and start coding: npm run remove-demo</li>
+        </ol>
+      </div>
+    );
+  }
+}
+
+HomePage.propTypes = {
 };
+
 
 export default HomePage;
