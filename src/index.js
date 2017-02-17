9e1615ee0679e6a4ce20e749fbc0c0b7da8f08e6
Silin Na <n1207n@gmail.com>
2017-02-17 15:48:40 -0800

    feat: Replaced React-bootstrap with material-ui

--- src/index.js
+++ src/index.js
@@ -2,18 +2,52 @@
 
 import React from 'react';
 import {render} from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
-import routes from './routes';
-import configureStore from './store/configureStore';
-require('./favicon.ico'); // Tell webpack to load favicon.ico
-import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-import { syncHistoryWithStore } from 'react-router-redux';
 
-// Load bootstrap & font-awesome CSS modules
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
+import {syncHistoryWithStore} from 'react-router-redux';
+
+require('./favicon.ico');
+import './styles/styles.scss';
 import 'font-awesome/css/font-awesome.css';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {
+  teal500,
+  teal700,
+  grey400,
+  redA200,
+  grey100,
+  grey500,
+  darkBlack,
+  white,
+  grey300,
+  fullBlack
+} from 'material-ui/styles/colors';
+
+injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: teal500,
+    primary2Color: teal700,
+    primary3Color: grey400,
+    accent1Color: redA200,
+    accent2Color: grey100,
+    accent3Color: grey500,
+    textColor: darkBlack,
+    alternateTextColor: white,
+    canvasColor: white,
+    borderColor: grey300,
+    pickerHeaderColor: teal500,
+    shadowColor: fullBlack
+  }
+});
 
 const store = configureStore();
 
@@ -21,7 +55,8 @@ const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
 
 render(
+  <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
   <Provider store={store}>
-    <Router history={history} routes={routes} />
-  </Provider>, document.getElementById('app')
-);
+    <Router history={history} routes={routes}/>
+  </Provider>
+</MuiThemeProvider>, document.getElementById('app'));
